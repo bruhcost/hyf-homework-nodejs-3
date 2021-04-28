df49420f221efddb508d53501ced045967eebfbc
bruhcost <83356452+bruhcost@users.noreply.github.com>
2021-04-28 14:33:05 -0300

    add "/users" POST route

--- server.js
+++ server.js
@@ -4,5 +4,6 @@ const app = express()
 
 app.get('/', (req, res)=> res.status(200).send('Hello World'))
 app.get('/users', (req, res)=>res.status(200).json([]))
+app.get('/users/:id', (req, res)=> res.status(200).json({ sucess: true, message:'usuário encontrado', user:{name: req.params.id}}))
 
 app.listen(8000, () => console.log('Server is running'))
