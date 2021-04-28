278bfcb252a078f6e2155ca1fda1b8ea25b82706
bruhcost <83356452+bruhcost@users.noreply.github.com>
2021-04-28 14:41:41 -0300

    add "/users/:id" DELETE route

--- server.js
+++ server.js
@@ -5,5 +5,6 @@ const app = express()
 app.get('/', (req, res)=> res.status(200).send('Hello World'))
 app.get('/users', (req, res)=>res.status(200).json([]))
 app.get('/users/:id', (req, res)=> res.status(200).json({ sucess: true, message:'usuário encontrado', user:{name: req.params.id}}))
+app.delete('/users/:id', (req, res)=> res.status(200).json({}))
 
 app.listen(8000, () => console.log('Server is running'))
