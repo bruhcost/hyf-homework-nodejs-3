0971b8fc266de3003c03129c21352fd78c6b3ae2
bruhcost <83356452+bruhcost@users.noreply.github.com>
2021-04-28 14:26:45 -0300

    add "/" GET route

--- server.js
+++ server.js
@@ -2,4 +2,6 @@
 const express = require('express')
 const app = express()
 
+app.get('/', (req, res)=> res.status(200).send('Hello World'))
+
 app.listen(8000, () => console.log('Server is running'))
