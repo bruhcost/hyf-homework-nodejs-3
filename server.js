bc6cd279bfe36ee6f62356295c5c3d3f974a1449
bruhcost <83356452+bruhcost@users.noreply.github.com>
2021-04-28 14:27:04 -0300

    add "/users" GET route

--- server.js
+++ server.js
@@ -3,5 +3,6 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res)=> res.status(200).send('Hello World'))
+app.get('/users', (req, res)=>res.status(200).json([]))
 
 app.listen(8000, () => console.log('Server is running'))
